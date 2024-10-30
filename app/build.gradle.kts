9f1d40f72241fea591d661f4af6e9b08fa51d865
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-30 10:56:54 +0700

    Update Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
+    buildFeatures {
+        viewBinding = true
+    }
 
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    dependencies {
+        val work_version = "2.8.1"
+        implementation("androidx.work:work-runtime:$work_version")
+        implementation("androidx.work:work-runtime-ktx:$work_version")
+        implementation("com.airbnb.android:lottie:5.2.0")
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+    }
 }
